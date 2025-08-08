41292924c31ba55abe12b7a6553d5b805599ea70
tiagosousa <tiagosousa.tams@hotmail.com>
2025-08-08 08:10:55 +0100

    test signers and real estates addresses

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,10 +1,22 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe('Escrow', () => {
+describe("Escrow", () => {
+  it("saves the addresses", async () => {
+    const signers = await ethers.getSigners();
+    console.log("🚀 ~ signers:", signers);
 
-})
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    realEstate = await RealEstate.deploy();
+    console.log("🚀 ~ realEstate:", realEstate.address);
+
+    //mint
+    // let transaction = await realEstate.mint(
+    //   "https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/1.png"
+    // );
+  });
+});
