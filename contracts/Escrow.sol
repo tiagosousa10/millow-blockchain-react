84b5f5a371637562a37ba0b9cc44489c6c42f07f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-08-08 08:35:05 +0100

    test if updates ownership

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -22,4 +22,8 @@ contract Escrow {
         inspector = _inspector;
         lender = _lender;
     }
+
+    function list(uint256 _nftID) public {
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -33,6 +33,14 @@ describe("Escrow", () => {
       inspector.address,
       lender.address
     );
+
+    // Approve Property
+    transaction = await realEstate.connect(seller).approve(escrow.address, 1);
+    await transaction.wait();
+
+    // List Property
+    transaction = await escrow.connect(seller).list(1);
+    await transaction.wait();
   });
 
   describe("Deployment", () => {
@@ -54,6 +62,12 @@ describe("Escrow", () => {
     it("Returns lender", async () => {
       const result = await escrow.lender();
       expect(result).to.be.equal(lender.address);
+    });
+  });
+
+  describe("Listing", () => {
+    it("Updates ownership", async () => {
+      expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address);
     });
   });
 });
